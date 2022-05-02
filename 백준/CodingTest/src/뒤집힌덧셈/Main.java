c074d27a02d52a24b2c35d12b0a1c292d46443e4
[haezzang] <songna89@naver.com>
2022-05-02 19:53:24 +0900

    뒤집힌 덧셈

---
+++ 백준/CodingTest/src/뒤집힌덧셈/Main.java
@@ -0,0 +1,27 @@
+package µÚÁýÈùµ¡¼À;
+
+import java.util.Scanner;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+
+		String s1 = scan.next();
+
+		String s2 = scan.next();
+
+
+		int a = Integer.parseInt( new StringBuffer(s1).reverse().toString());
+		int b = Integer.parseInt( new StringBuffer(s2).reverse().toString());
+		
+		String res=new StringBuffer( Integer.toString(a+b)).reverse().toString();
+	
+
+		System.out.println(Integer.parseInt(res));
+
+	}
+
+}
